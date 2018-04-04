1d6b8e379bad57705caa170f8ec51f3487729618
killersshadow <32833605+killersshadow@users.noreply.github.com>
2018-04-04 09:19:39 +0000

    Add files via upload

---
+++ form4_err/views/from.php
@@ -0,0 +1,16 @@
+<h1>Contactformulier</h1>
+<h3>Joran GD1B</h3>
+<hr>
+<pre>
+  <form method="post">
+
+    naam:    <input type="text" name="name" autofocus required>
+
+    email:   <input type="email" name="email" required>
+
+    bericht: <input type="text" name="message" required>
+
+             <input type="submit" name='sendContact' required>
+  </form>
+</pre>
+<hr>
